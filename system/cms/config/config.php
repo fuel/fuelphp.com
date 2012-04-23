32a08bdaf10d9bb91730b0b8c2ead675dc9a3cd8
Phil Sturgeon <email@philsturgeon.co.uk>
2012-04-23 14:16:35 +0100

    Tweaks.

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://fuelphp.com/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
